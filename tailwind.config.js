2be09e910a237bf59469da2523d5663a63438c1f
elfatrushiti-hash <elfat.rushiti@gmail.com>
2026-03-18 00:16:40 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,16 @@
-// tailwind.config.js
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        primaryPurple: '#8750E5',   // Primärfarbe Purple 60
-        white: '#FFFFFF',            // Primärfarbe Weiß
-        black: '#000000',            // Primärfarbe Schwarz
-
+        primaryPurple: '#8750E5',
+        white: '#FFFFFF',
+        black: '#000000',
         bgPurple100: '#29074A',
         bgPurple80: '#4F1D8D',
         bgPurple40: '#C9B0EF',
         bgPurple20: '#F1E8FA',
         bgGrey20: '#F5F4F2',
-
         chartAccent1: '#170456',
         chartAccent2: '#1316C7',
         chartAccent3: '#2395FF',
