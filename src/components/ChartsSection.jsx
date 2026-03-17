70ccf26e1b3762707c4cff615ec1c70a47184ecf
elfatrushiti-hash <elfat.rushiti@gmail.com>
2026-03-18 00:43:51 +0100

    Update ChartsSection.jsx

--- src/components/ChartsSection.jsx
+++ src/components/ChartsSection.jsx
@@ -1,8 +1,8 @@
-import React from "react"
-import { Doughnut } from "react-chartjs-2"
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js"
+import React from "react";
+import { Doughnut } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 
-ChartJS.register(ArcElement, Tooltip, Legend)
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 export default function ChartsSection({ data }) {
   const chartData = {
@@ -15,12 +15,12 @@ export default function ChartsSection({ data }) {
         borderWidth: 1
       }
     ]
-  }
+  };
 
   return (
     <div className="bg-white p-6 rounded-lg shadow text-center">
       <h3 className="text-lg font-bold mb-4 text-[#8750E5]">Kostenvergleich</h3>
-      <Doughnut id="chart-doughnut" data={chartData} />
+      <Doughnut className="chart-canvas" data={chartData} />
     </div>
-  )
+  );
 }
